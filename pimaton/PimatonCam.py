0ae463133d241eaec07e6d6e3e2566444962f246
Raphael Khaiat <raphael@khaiat.org>
2018-01-11 19:47:07 +0100

    Add picamera configuration and implements #17

--- pimaton/PimatonCam.py
+++ pimaton/PimatonCam.py
@@ -25,6 +25,9 @@ class PimatonCam:
 	"""
         logger.debug("Starting taking picture")
         taken_pictures = []
+        self.picamera.start_preview()
+        sleep(self.config['time_before_first_picture'])
+
         for i in range(self.config['number_of_pictures_to_take']):  
             filename = self.config['picture_prefix_name'] + "_" + unique_key + '_' + datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + ".jpg"
             taken_pictures.append(filename)
@@ -33,6 +36,7 @@ class PimatonCam:
 	    self.capture(filename)
             logger.debug("Photo (" + filename + ") saved: " + filename)
 
+        self.picamera.stop_preview()
         logger.debug("The following pictures were taken %s" % taken_pictures)
         return taken_pictures
 
@@ -46,3 +50,19 @@ class PimatonCam:
         logger.debug('No config passed, default config used')
         # TODO v0.0.1 : Manage camera configuration.
 
+        # self.picamera.resolution            = (1920, 1440)
+        self.picamera.resolution            = (640, 480)
+        # self.picamera.framerate             = 24
+        # self.picamera.sharpness             = 0
+        # self.picamera.contrast              = 0
+        # self.picamera.brightness            = 50
+        # self.picamera.saturation            = 0
+        # self.picamera.iso                   = 0
+        # self.picamera.video_stabilization   = False
+        # self.picamera.exposure_compensation = 0
+        self.picamera.exposure_mode         = 'auto'
+        self.picamera.meter_mode            = 'average'
+        self.picamera.awb_mode              = 'auto'
+        self.picamera.rotation              = 0
+        self.picamera.hflip                 = True
+        self.picamera.vflip                 = False
