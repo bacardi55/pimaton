fa573ca5efc4339383e27a3dd82c519ae3fe8fe9
Raphael Khaiat <raphael@khaiat.org>
2018-01-11 19:48:05 +0100

    Create final pictures base on a template or a new blank image with thumbnails. Implements #3

--- pimaton/Pimaton.py
+++ pimaton/Pimaton.py
@@ -36,7 +36,7 @@ class Pimaton:
 
             # TODO v0.0.1 : Manage exception.
             filename = self.config['picamera']['generated_prefix_name'] + '_' + unique_key + '_' + datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + ".jpg"
-            to_print = PimatonImage().render_image_to_print(taken_pictures, self.config['picamera']['photo_directory'], filename)
+            to_print = PimatonImage().render_image_to_print(self.__get_fullpath_thumbnails_list(taken_pictures), filename, self.config['image'])
 
             # TODO v0.0.3 : Manage print.
             # pimatonprint = PimatonPrint()
@@ -53,16 +53,37 @@ class Pimaton:
         config = {
             'picamera': {
                 'photo_directory': '/home/pi/pimaton_pictures',
-                'number_of_pictures_to_take': 3,
-                'time_before_first_picture': 4,
-                'time_between_pictures': 2,
+                'number_of_pictures_to_take': 6,
+                'time_before_first_picture': 2,
+                'time_between_pictures': 1,
                 'picture_prefix_name': 'test_pimaton',
                 'generated_prefix_name': 'pimaton_'
             },
             'pimaton': {
                 'time_between_loop': 2
+            },
+            'image': {
+                'print_pic': {
+                    'template': None,
+                    'output_dir': '/home/pi/pimaton_pictures',
+                    'width': 1754,
+                    'height': 1241,
+                    'rows': 3,
+                    'cols': 2
+                },
+                'thumbnails': {
+                    'width': 560,
+                    'height': 496
+                },
             }
         }
+
         logging.debug('Loading default config %s' % config)
         self.config = config
         
+    def __get_fullpath_thumbnails_list(self, taken_pictures):
+        fpp = []
+        for pic in taken_pictures:
+            fpp.append(self.config['picamera']['photo_directory'] + '/' + pic)
+
+        return fpp
--- pimaton/PimatonImage.py
+++ pimaton/PimatonImage.py
@@ -2,6 +2,9 @@ from six import with_metaclass
 from Singleton import Singleton
 import logging
 
+from PIL import Image, ImageDraw, ImageFont
+import math
+
 logging.basicConfig()
 logger = logging.getLogger("Pimaton")
 
@@ -12,8 +15,62 @@ class PimatonImage(with_metaclass(Singleton, object)):
     def __init__(self):
         logger.debug('Instantiate PimatonImage')
 
-    def render_image_to_print(self, taken_pictures, output_dir, filename):
-        logger.info('Generating image to be printed: %s/%s' % (output_dir, filename))
+    def render_image_to_print(self, taken_pictures, filename, config):
+        """
+        This method create the final image to be printed.
+        """
+
+        logger.info('Generating image to be printed: %s/%s' % (config['print_pic']['output_dir'], filename))
         logger.debug('Taken Pictures: %s' % taken_pictures)
-        # TODO v0.0.1 : Create the 1 picture with the taken picture.
-        # TODO v0.0.1 : Benchamark between using Pillow library or an imagemagick wrapper.
+
+        dimensions = self.__calculate_dimensions(config)
+
+        thumbnails = []
+        for file in taken_pictures:
+            try:
+                # TODO: change path via config.
+                im = Image.open(file)
+                im.thumbnail(dimensions['thumbnail_size'])
+                thumbnails.append(im)
+                # im.save('./thumbnail_' + file, "JPEG")
+
+            except IOError as e:
+                # TODO
+                print("Error: %s" % e)
+
+        if config['print_pic']['template'] is not None:
+            # TODO: Check if file exists or raise exception.
+            generated = Image.open(config['print_pic']['template'])
+        else:    
+            generated = Image.new('RGB', dimensions['print_pic_size'], 'white')
+
+        for idx, thumbnail in enumerate(thumbnails):
+            current_col = idx % config['print_pic']['cols'] + 1
+            current_row = int(math.floor((idx / config['print_pic']['cols']))) + 1
+            x_position = (current_col * dimensions['x_border']) + ((current_col - 1) * config['thumbnails']['width']) 
+            y_position = (current_row * dimensions['y_border']) + ((current_row - 1) * config['thumbnails']['height'])
+            generated.paste(thumbnail, (x_position, y_position))
+     
+            # TODO: TMP code to be removed.
+            tmp = ImageDraw.Draw(generated)
+            tmp.rectangle([(x_position, y_position), (x_position + config['thumbnails']['width'], y_position + config['thumbnails']['height'])], outline="black")
+
+        
+        # TODO: change path via config.
+        generated.save(config['print_pic']['output_dir'] + '/' + filename, 'JPEG')
+        logger.debug('Final picture has been generated')
+
+
+    def __calculate_dimensions(self, config):
+        """
+        This private method calculate different variables needed to create the final image
+        """
+        dim = {
+            'thumbnail_size': (config['thumbnails']['width'], config['thumbnails']['height']),
+            'print_pic_size': (config['print_pic']['width'], config['print_pic']['height']),
+            'x_border': (config['print_pic']['width'] - config['print_pic']['cols'] * config['thumbnails']['width']) / (config['print_pic']['cols'] + 1),
+            'y_border': (config['print_pic']['height'] - config['print_pic']['rows'] * config['thumbnails']['height']) / (config['print_pic']['rows'] + 1)
+        }
+
+        logger.debug('Calculated dimensions: %s' % dim)
+        return dim
