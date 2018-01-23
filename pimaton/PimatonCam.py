83488233b31a268464974d867d3352fb78b6eb1b
Raphael Khaiat <raphael@khaiat.org>
2018-01-23 23:30:23 +0100

    Implement very simple overlay for picamera #30

--- pimaton/PimatonCam.py
+++ pimaton/PimatonCam.py
@@ -27,23 +27,30 @@ class PimatonCam:
         logger.debug("Starting taking_pictures")
 
         taken_pictures = []
+
         self.picamera.start_preview()
-        sleep(self.config['time_before_first_picture'])
+        self.countdown(self.config['time_before_first_picture'])
 
         for i in range(self.config['number_of_pictures_to_take']):
             filename = self.config['picture_prefix_name'] + "_" + unique_key + \
                 '_' + datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + ".jpg"
             taken_pictures.append(filename)
 
-            sleep(self.config['time_between_pictures'])
             logger.debug('Taking picture %s' % i)
             self.capture(filename)
             logger.debug("Photo (" + filename + ") saved: " + filename)
+            self.countdown(self.config['time_between_pictures'])
 
         self.picamera.stop_preview()
 
         logger.debug("The following pictures were taken %s" % taken_pictures)
         return taken_pictures
+
+    def countdown(self, sec):
+        for i in range(0, sec):
+            self.picamera.annotate_text =  str(sec - i)
+            sleep(1)
+        self.picamera.annotate_text = ''
 
     def capture(self, filename):
         logger.debug("Capturing picture %s in %s" %
@@ -76,5 +83,8 @@ class PimatonCam:
             self.picamera.rotation = config['settings']['rotation']
             self.picamera.hflip = config['settings']['hflip']
             self.picamera.vflip = config['settings']['vflip']
+
+            self.picamera.annotate_text = ''
+            self.picamera.annotate_text_size = 80
         except Exception as e:
             raise PimatonCamExceptions('Couldnt config picamera: %s' % e)
